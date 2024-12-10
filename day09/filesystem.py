6ff3300cec95488e8fbf7809c117d13033f439e7
Eric Yager <ejyager00@protonmail.com>
2024-12-10 11:25:40 -0500

    Solve day nine part two

--- day09/filesystem.py
+++ day09/filesystem.py
@@ -13,6 +13,13 @@ def construct_filesystem(diskMap: str) -> list[int]:
             filesys += [int(i/2)]*int(x)
     return filesys
 
+def compute_checksum(data: list[int]) -> int:
+    checksum = 0
+    for i, x in enumerate(data):
+        if x != -1:
+            checksum += i*x
+    return checksum
+
 def frontload_files(filesys: list[int]):
     p1 = 0
     p2 = len(filesys)-1
@@ -24,23 +31,46 @@ def frontload_files(filesys: list[int]):
         if p1<p2:
             filesys[p1] = filesys[p2]
             filesys[p2] = -1
-    return
 
-def compute_checksum(data: list[int]) -> int:
-    checksum = 0
-    for i, x in enumerate(data):
-        if x != -1:
-            checksum += i*x
-    return checksum
+def frontload_whole_files(filesys: list[int], file_sizes: list[int], spaces: list[int]):
+    files = [(i, size) for i, size in enumerate(file_sizes)]
+    current_pos = 0
+    
+    for file_id in range(len(files)-1, -1, -1):
+        file_size = file_sizes[file_id]
+        
+        file_start = None
+        for i in range(len(filesys)):
+            if filesys[i] == file_id:
+                file_start = i
+                break
+                
+        if file_start is None:
+            continue
+            
+        space_start = None
+        for i in range(len(filesys)):
+            if filesys[i] == -1:
+                if all(filesys[j] == -1 for j in range(i, min(i + file_size, len(filesys)))):
+                    space_start = i
+                    break
+                    
+        if space_start is not None and space_start < file_start:
+            for i in range(file_size):
+                filesys[space_start + i] = file_id
+                filesys[file_start + i] = -1
 
 def get_frontloaded_checksum(diskMap: str) -> int:
-    # print(diskMap)
     filesys = construct_filesystem(diskMap)
-    # print(''.join(['.' if x==-1 else str(x) for x in filesys]))
     frontload_files(filesys)
-    # print(''.join(['.' if x==-1 else str(x) for x in filesys]))
     return compute_checksum(filesys)
 
+def get_unfrag_front_checksum(diskMap: str) -> int:
+    file_sizes = [int(x) for x in diskMap[::2]]
+    spaces = [int(x) for x in diskMap[1::2]]
+    filesys = construct_filesystem(diskMap)
+    frontload_whole_files(filesys, file_sizes, spaces)
+    return compute_checksum(filesys)
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
@@ -51,6 +81,7 @@ if __name__ == '__main__':
 
     try:
         diskMap = parse_input(input_file)
-        print(get_frontloaded_checksum(diskMap))
+        print(f"Checksum for frontloaded system is      {get_frontloaded_checksum(diskMap)}")
+        print(f"Checksum while not fragmenting files is {get_unfrag_front_checksum(diskMap)}")
     except FileNotFoundError:
         print(f"Error: Could not find input file '{input_file}'")
