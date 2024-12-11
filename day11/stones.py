87de37ad1f6a5eaee560d50f3a444117fc143307
Eric Yager <ejyager00@protonmail.com>
2024-12-11 09:58:18 -0500

    Refactor day 11 to use functools

--- day11/stones.py
+++ day11/stones.py
@@ -1,43 +1,41 @@
 import sys
+from functools import lru_cache, reduce
 
-# {(stone_label, blinks): stone_count}
-stone_cache = {(0, 1): 1}
-
-def parse_input(filename: str) -> list[list[int]]:
+def parse_input(filename: str) -> tuple[int]:
     with open(filename) as f:
-        return [int(x) for x in f.read().split(' ')]
+        return tuple([int(x) for x in f.read().split(' ')])
 
-def apply_rule(stone: int) -> list[int]:
-    if stone==0:
-        return [1]
-    stone_string = str(stone)
-    digits = len(stone_string)
-    if digits%2==0:
-        return [int(stone_string[:digits//2]), int(stone_string[digits//2:])]
-    return [stone*2024]
+def split_even_digits(stone_str: str) -> tuple[int]:
+    digits = len(stone_str)
+    return (int(stone_str[:digits//2]), int(stone_str[digits//2:]))
 
-def blink(stones: list[int], blinks: int=1) -> int:
-    if blinks==0:
+def transform_stone(stone: int) -> tuple[int]:
+    stone_str = str(stone)
+    return split_even_digits(stone_str) if len(stone_str) % 2 == 0 else (stone * 2024,)
+
+def apply_stone_rule(stone: int) -> tuple[int]:
+    return (1,) if stone == 0 else transform_stone(stone)
+
+@lru_cache(maxsize=None)
+def num_stones_after_blinks(stones: tuple[int, ...], blinks: int = 1) -> int:
+    if blinks == 0:
         return len(stones)
-    if len(stones) > 1:
-        return sum([blink([s], blinks=blinks) for s in stones])
-    stone = stones[0]
-    key = (stone, blinks)
-    if key not in stone_cache:
-        stone_cache[key] = blink(apply_rule(stone), blinks=blinks-1)
-    return stone_cache[key]
     
+    count_single = lambda stone: num_stones_after_blinks(apply_stone_rule(stone), blinks - 1)
+    
+    return reduce(lambda acc, stone: acc + count_single(stone), stones, 0) if len(stones) > 1 else count_single(stones[0])
+
+def main(stones: tuple[int, ...]) -> None:
+    print(f"Stones after 25 blinks: {num_stones_after_blinks(stones, blinks=25)}")
+    print(f"Stones after 75 blinks: {num_stones_after_blinks(stones, blinks=75)}")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: python checkReports.py <input_file>")
         sys.exit(1)
-        
-    input_file = sys.argv[1]
 
     try:
-        stone_list = parse_input(input_file)
-        print(f"Stones after 25 blinks: {blink(stone_list, blinks=25)}")
-        print(f"Stones after 75 blinks: {blink(stone_list, blinks=75)}")
+        stones = parse_input(sys.argv[1])
+        main(stones)
     except FileNotFoundError:
         print(f"Error: Could not find input file '{input_file}'")
