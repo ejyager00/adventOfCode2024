95c1d4fecbe9330537e6d835c0fbc97773342599
Eric Yager <ejyager00@protonmail.com>
2024-12-16 11:11:37 -0500

    Day fifteen part two, works on example, doesn't work on real input, giving up for now

--- day15/warehouserobot.py
+++ day15/warehouserobot.py
@@ -36,10 +36,63 @@ def move_robot(warehouse: list[list[str]], move: str, robot: tuple[int, int]) ->
     if neighbor == "#":
         return robot
     if shifting>1:
-        warehouse[robot[0]+shifting*dyx[0]][robot[1]+shifting*dyx[1]]="O"
+        for i in range(shifting, 1, -1):
+            warehouse[robot[0]+i*dyx[0]][robot[1]+i*dyx[1]]=warehouse[robot[0]+(i-1)*dyx[0]][robot[1]+(i-1)*dyx[1]]
     warehouse[robot[0]][robot[1]]="."
     warehouse[robot[0]+dyx[0]][robot[1]+dyx[1]]="@"
     return robot[0]+dyx[0], robot[1]+dyx[1]
+
+def find_moved_boxes(warehouse: list[list[str]], box: tuple[int, int], dy: int) -> tuple[list[tuple[int, int]], bool]:
+    if warehouse[box[0]+dy][box[1]]=="#" or warehouse[box[0]+dy][box[1]+1]=="#":
+        return None, True
+    if warehouse[box[0]+dy][box[1]]=="[":
+        boxes, blocked = find_moved_boxes(warehouse, (box[0]+dy,box[1]), dy)
+        if blocked:
+            return None, True
+        return [box]+boxes, False
+    boxes = [box]
+    if warehouse[box[0]+dy][box[1]]=="]":
+        left_boxes, blocked = find_moved_boxes(warehouse, (box[0]+dy,box[1]-1), dy)
+        if blocked:
+            return None, True
+        boxes+=left_boxes
+    if warehouse[box[0]+dy][box[1]+1]=="[":
+        right_boxes, blocked = find_moved_boxes(warehouse, (box[0]+dy,box[1]+1), dy)
+        if blocked:
+            return None, True
+        boxes+=right_boxes
+    return boxes, False
+
+def move_robot_expanded(warehouse: list[list[str]], move: str, robot: tuple[int, int]) -> tuple[int, int]:
+    print_warehouse(warehouse)
+    print()
+    dyx = get_direction(move)
+    if dyx[0]==0:
+        return move_robot(warehouse, move, robot)
+    loc = robot
+    dy = dyx[0]
+    if warehouse[loc[0]+dy][loc[1]]=="#":
+        return robot
+    if warehouse[loc[0]+dy][loc[1]]==".":
+        warehouse[robot[0]][robot[1]]="."
+        warehouse[robot[0]+dy][robot[1]]="@"
+        return robot[0]+dy, robot[1]
+    start = (loc[0]+dy, loc[1]) if warehouse[loc[0]+dy][loc[1]]=="[" else (loc[0]+dy, loc[1]-1)
+    boxes, blocked = find_moved_boxes(warehouse, start, dy)
+    if blocked:
+        return robot
+    for box in boxes[::-1]:
+        warehouse[box[0]][box[1]]="."
+        warehouse[box[0]][box[1]+1]="."
+        warehouse[box[0]+dy][box[1]]="["
+        warehouse[box[0]+dy][box[1]+1]="]"
+    warehouse[robot[0]][robot[1]]="."
+    warehouse[robot[0]+dy][robot[1]]="@"
+    if start[1]==robot[1]:
+        warehouse[robot[0]+dy][robot[1]+1]="."
+    else:
+        warehouse[robot[0]+dy][robot[1]-1]="."
+    return robot[0]+dy, robot[1]
 
 def sum_gps(warehouse: list[list[str]]) -> int:
     total = 0
@@ -52,10 +105,11 @@ def sum_gps(warehouse: list[list[str]]) -> int:
 def expand_warehouse(warehouse: list[list[str]]) -> list[list[str]]:
     return [[x for x in "".join(row).replace("#","##").replace("O","[]").replace(".","..").replace("@","@.")] for row in warehouse]
 
-def move_and_tally(warehouse: list[list[str]], moves: tuple[str, ...]) -> int:
+def move_and_tally(warehouse: list[list[str]], moves: tuple[str, ...], big: bool = False) -> int:
     loc = locate_robot(warehouse)
+    move = move_robot_expanded if big else move_robot
     for m in moves:
-        loc = move_robot(warehouse, m, loc)
+        loc = move(warehouse, m, loc)
     return sum_gps(warehouse)
 
 if __name__ == '__main__':
@@ -67,5 +121,7 @@ if __name__ == '__main__':
         warehouse, movements = parse_input(sys.argv[1])
         big_warehouse = expand_warehouse(warehouse)
         print(f"The sum of all boxes' GPS coordinates is {move_and_tally(warehouse, movements)}.")
+        print(f"The sum of all boxes' GPS coordinates in the big warehouse is {move_and_tally(big_warehouse, movements, big=True)}.")
+        print_warehouse(big_warehouse)
     except FileNotFoundError:
-        print(f"Error: Could not find input file '{input_file}'")
+        print(f"Error: Could not find input file '{sys.argv[1]}'")
