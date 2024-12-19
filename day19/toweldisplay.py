2a372daa0f66f1a894cd6ad2ecaf5ddf1723e4f8
Eric Yager <ejyager00@protonmail.com>
2024-12-19 08:40:31 -0500

    Solve day 19 part 2

--- day19/toweldisplay.py
+++ day19/toweldisplay.py
@@ -1,5 +1,5 @@
 import sys
-from functools import lru_cache, reduce
+from functools import lru_cache
 
 def parse_input(filename: str) -> tuple[tuple[str, ...], tuple[str, ...]]:
     with open(filename) as f:
@@ -7,26 +7,25 @@ def parse_input(filename: str) -> tuple[tuple[str, ...], tuple[str, ...]]:
         return (tuple(towels.split(", ")), tuple(displays.split("\n")))
 
 @lru_cache(maxsize=None)
-def display_is_possible(towels: tuple[str, ...], display: str) -> bool:
-    if len(display)==0 or display in towels:
-        return True
-
-    for towel in towels:
-        if display[:len(towel)]==towel and display_is_possible(towels, display[len(towel):]):
-            return True
-
-    return False
+def display_arrangements(towels: tuple[str, ...], display: str) -> int:
+    if display=="":
+        return 1
+    return sum([display_arrangements(towels, display[len(towel):]) for towel in towels if display[:len(towel)]==towel])
 
 def possible_designs(towels: tuple[str, ...], displays: tuple[str, ...]) -> int:
-    return sum([display_is_possible(towels, display) for display in displays])
+    return sum([display_arrangements(towels, display)>0 for display in displays])
+
+def total_unique_arrangments(towels: tuple[str, ...], displays: tuple[str, ...]) -> int:
+    return sum([display_arrangements(towels, display) for display in displays])
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print("Usage: python checkReports.py <input_file>")
+        print("Usage: python toweldisplay.py <input_file>")
         sys.exit(1)
 
     try:
         towels, displays = parse_input(sys.argv[1])
         print(f"The number of possible designs is {possible_designs(towels, displays)}")
+        print(f"The number of unique design arrangements is {total_unique_arrangments(towels, displays)}")
     except FileNotFoundError:
         print(f"Error: Could not find input file '{sys.argv[1]}'")
