65b3575abc6eebda1c6f4f0cc16a3c72e579f819
Eric Yager <ejyager00@protonmail.com>
2024-12-20 10:19:52 -0500

    solve day twenty

--- day20/racecondition.py
+++ day20/racecondition.py
@@ -32,18 +32,20 @@ def get_track_positions(racetrack: list[str], start: tuple[int, int], end: tuple
         track_positions[position] = len(track_positions)
     return track_positions
 
-def get_cheats(racetrack: list[str], min_saving: int) -> int:
+def taxi_dist(pos1: tuple[int, int], pos2: tuple[int, int]) -> int:
+    return abs(pos1[0]-pos2[0])+abs(pos1[1]-pos2[1])
+
+def get_cheats(racetrack: list[str], min_saving: int, cheat_dist: int = 2) -> int:
     start, end = get_start_and_end(racetrack)
     racetrack[start[0]] = racetrack[start[0]].replace('S','.')
     racetrack[end[0]] = racetrack[end[0]].replace('E','.')
     track_positions = get_track_positions(racetrack,start, end)
     cheats = 0
-    for ty, tx in track_positions:
-        for y, x in DIRECTIONS:
-            if track_positions.get((ty+2*y, tx+2*x),-1)>track_positions[(ty,tx)] and (ty+y, tx+x) not in track_positions:
-                time_saved = track_positions[(ty+2*y, tx+2*x)]-track_positions[(ty,tx)]-2
-                # print(f"cheating from {(ty,tx)} to {(ty+2*y, tx+2*x)} saves {time_saved} picoseconds")
-                cheats += 1 if time_saved>=min_saving else 0
+    for i, (ty, tx) in enumerate(track_positions):
+        for oy, ox in list(track_positions)[i+1:]:
+            if 1<taxi_dist((ty,tx),(oy,ox))<=cheat_dist:
+                time_saved = track_positions[(oy,ox)]-track_positions[(ty,tx)]-taxi_dist((ty,tx),(oy,ox))
+                cheats += 1 if time_saved >= min_saving else 0
     return cheats
         
 if __name__ == '__main__':
@@ -55,5 +57,8 @@ if __name__ == '__main__':
         MIN_SAVINGS = 100
         lines = parse_input(sys.argv[1])
         print(f"The number of ways to save at least {MIN_SAVINGS} picoseconds is {get_cheats(lines, MIN_SAVINGS)}")
+        CHEAT_LENGTH = 20
+        lines = parse_input(sys.argv[1])
+        print(f"The number of ways to save at least {MIN_SAVINGS} picoseconds with longer cheats is {get_cheats(lines, MIN_SAVINGS, cheat_dist=CHEAT_LENGTH)}")
     except FileNotFoundError:
         print(f"Error: Could not find input file '{sys.argv[1]}'")
